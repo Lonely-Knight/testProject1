384e1f77395e2f9110ffe63c9b08f90efa7ecc8b
Eugene Gurin <44319878+Lonely-Knight@users.noreply.github.com>
2023-11-21 21:44:06 +0300

    Update projectPython1.py

--- projectPython1.py
+++ projectPython1.py
@@ -1,14 +1,17 @@
 import re
 
-
+# функция по добавлению нулей слева от числа
 def convert_number(match):
     number = match.group()
     parts = number.split('\\')
+    # добавляет нули в левой части от backslash
     left_part = parts[0].zfill(4)
+    # добавляет нули в правой части от backslash
     right_part = parts[1].zfill(5)
+    # печать измененного номера
     print(f"{left_part}\\{right_part}")
 
-
+# поиск искомых данных по regex и отправка в функцию для исправления
 def format_numbers(input_str):
     pattern = r'\d{2,4}\\\d{2,5}'
     re.sub(pattern, convert_number, input_string)
