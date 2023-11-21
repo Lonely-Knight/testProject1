43e6493c950f4dd3b531cc12c366916ab838abe6
Eugene Gurin <44319878+Lonely-Knight@users.noreply.github.com>
2023-11-21 21:46:32 +0300

    Update projectPython3.py

--- projectPython3.py
+++ projectPython3.py
@@ -1,10 +1,10 @@
 from functools import cmp_to_key
 
-
+# функция сравнения рядом находящихся чисел
 def comparison(a, b):
     return int(b + a) - int(a + b)
 
-
+# функция сортировки и объединения отсортированного массива входных данных в наибольшее число
 def max_number(str_list):
     sorted_numbers = sorted(str_list, key=cmp_to_key(comparison))
     return ''.join(sorted_numbers)
